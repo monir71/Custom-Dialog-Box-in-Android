35b78629dcdb5dd22342e444ef8f4f40b31f0b3b
monir71 <zamanmonir71@yahoo.com>
2025-06-14 16:45:51 +0600

    Cancelable

---
+++ app/src/main/java/com/example/customdialogbox/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.customdialogbox;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog_box);
+        dialog.show();
+        dialog.setCancelable(false);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
